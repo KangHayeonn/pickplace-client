750be2ad81224ba3c04b6f2dd49819ea56a30f5b
mingmeng030 <djwls_0716@naver.com>
2023-06-12 22:28:10 +0900

    [UPDATE]: remove unnecessary import

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './styles/style.scss';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
 import Router from './router/Router';
 
 const App: React.FC = () => {
