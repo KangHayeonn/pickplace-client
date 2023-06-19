888ed574c09b96b15c109ca1ea72702e0d0fd7d8
yeoni sugar <rkdgkdus522@naver.com>
2023-06-19 12:57:12 +0900

    [UPDATE] : 불필요한 코드 삭제

--- front/src/components/common/TextButton.tsx
+++ front/src/components/common/TextButton.tsx
@@ -4,28 +4,15 @@ import '../../styles/components/common/textButton.scss';
 interface ButtonProps {
   text?: string | undefined;
   disabled?: boolean | undefined;
-  fontSize?: number | undefined;
-  background?: string | undefined;
-  color?: string | undefined;
-  width?: number | undefined;
-  height?: number | undefined;
-  weight?: number | undefined;
-  border?: boolean | undefined;
   onClick: React.MouseEventHandler<HTMLElement>;
 }
 
-const TextButton = ({
-  text,
-  onClick,
-  disabled = false,
-  ...props
-}: ButtonProps) => {
+const TextButton = ({ text, onClick, disabled = false }: ButtonProps) => {
   return (
     <button
       className="text-btn secondary short"
       onClick={onClick}
       disabled={disabled}
-      {...props}
     >
       {text}
     </button>
