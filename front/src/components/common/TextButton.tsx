9c174f676de0b08b9ca903a5d3f7ef2e527fcab6
yeoni sugar <rkdgkdus522@naver.com>
2023-06-21 01:29:28 +0900

    [ADD] : 로그인 페이지 마크업 구현

--- front/src/components/common/TextButton.tsx
+++ front/src/components/common/TextButton.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import '../../styles/components/common/textButton.scss';
 
 interface ButtonProps {
+  type?: 'button' | 'submit' | 'reset' | undefined;
   text?: string | undefined;
   disabled?: boolean | undefined;
   classType?: string | undefined; // default: basic
-  onClick: React.MouseEventHandler<HTMLElement>;
+  onClick?: React.MouseEventHandler<HTMLElement>;
 }
 
 const TextButton = ({
+  type = 'button',
   text,
   disabled = false,
   classType,
@@ -16,6 +18,7 @@ const TextButton = ({
 }: ButtonProps) => {
   return (
     <button
+      type={type}
       className={`text-btn ${classType}`}
       onClick={onClick}
       disabled={disabled}
---
+++ front/src/pages/PasswordPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PasswordPage = () => {
+  return <div>비밀번호 찾기/변경 페이지</div>;
+};
+
+export default PasswordPage;
